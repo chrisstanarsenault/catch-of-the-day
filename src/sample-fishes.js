18d47f47fdfb176d731c89a2b139b79ed73f2817
Chris Arsenault <chrisstanarsenault@gmail.com>
2019-12-08 01:20:39 -0500

    fixed halibutt image path

--- src/sample-fishes.js
+++ src/sample-fishes.js
@@ -2,7 +2,7 @@
 const fishes = {
   fish1: {
     name: "Pacific Halibut",
-    image: "./images/hali.jpg",
+    image: "/images/hali.jpg",
     desc:
       "Everyone’s favorite white fish. We will cut it to the size you need and ship it.",
     price: 1724,
